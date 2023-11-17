ceed88191fe2588b4653f7531a3e629abde0d7ec
mmcqueen <macall.mcqueen@viasat.com>
2023-11-16 17:06:21 -0800

    cleanup

--- format_python.sh
+++ format_python.sh
@@ -1,16 +1,3 @@
-# Ignoring these pylint warnings:
-# C0302: too many lines in module
-# C0330: wrong hanging indent
-#     - pylint doesn't like black's formatting style
-# R0902: too many instance attributes
-# R0903: too few public methods in class
-# R0904: too many public methods in class
-# R0913: too many arguments
-# W0703: catching too general exception
-# C0413: used when code and imports are mixed
-# W0707: 'raise' missing 'from'
-# W1514: Using open without explicitly specifying an encoding
-PYLINT_IGNORE_CODES='C0302,C0330,R0902,R0903,R0904,R0913,W0703,C0413,W0707,W1514,R0201'
 BAR="--------------------------------------------------------------------\n"
 CONFIG_FILE_PATH='/home/mmcqueen/formatter/pyproject.toml'
 
