eac633a6baff2e525e8712a53f50b4347ccc4820
mmcqueen <macall.mcqueen@viasat.com>
2024-07-15 12:51:21 -0700

    updates

--- format_python.sh
+++ format_python.sh
@@ -1,7 +1,6 @@
 BAR="--------------------------------------------------------------------\n"
 CONFIG_FILE_PATH='/home/mmcqueen/formatter/pyproject.toml'
 
-
 error_count=0
 
 # Use the -a flag to do everything; Without the -a flag, we only change the formatting
@@ -17,33 +16,33 @@ done
 if $all
 then
   echo "${BAR}sorting methods..."
-  /home/mmcqueen/.local/bin/ssort $1 2>&1
+  /home/mmcqueen/code/bin/ssort $1 2>&1
   error_count=$((error_count + $?))
 fi
 
 if $all
 then
   echo "${BAR}pre-formatting..."
-  /home/mmcqueen/.local/bin/ruff format --preview --config $CONFIG_FILE_PATH $1 2>&1
+  /home/mmcqueen/code/bin/ruff format --preview --config $CONFIG_FILE_PATH $1 2>&1
   error_count=$((error_count + $?))
 fi
 
 if $all
 then
   echo "${BAR}linting & fixing..."
-  /home/mmcqueen/.local/bin/ruff check --fix --unsafe-fixes --preview --config $CONFIG_FILE_PATH $1 2>&1
+  /home/mmcqueen/code/bin/ruff check --fix --unsafe-fixes --preview --config $CONFIG_FILE_PATH $1 2>&1
   error_count=$((error_count + $?))
 fi
 
 echo "${BAR}formatting..."
-/home/mmcqueen/.local/bin/ruff format --preview --config $CONFIG_FILE_PATH $1 2>&1
+/home/mmcqueen/code/bin/ruff format --preview --config $CONFIG_FILE_PATH $1 2>&1
 /usr/bin/sed "s/\"\"\"/'''/g" -i $1 2>&1  # changes ''' back to '''
 error_count=$((error_count + $?))
 
 if $all
 then
   echo "${BAR}checking docstrings..."
-  /home/mmcqueen/.local/bin/pydoclint --config $CONFIG_FILE_PATH $1 # 2>&1
+  /home/mmcqueen/code/bin/pydoclint --config $CONFIG_FILE_PATH $1 # 2>&1
   error_count=$((error_count + $?))
 fi
 
@@ -51,14 +50,14 @@ fi
 if $all
 then
   echo "${BAR}checking type hints..."
-  /home/mmcqueen/.local/bin/mypy --install-types --non-interactive --ignore-missing-imports --disallow-untyped-defs --disable-error-code union-attr $1 2>&1
+  /home/mmcqueen/code/bin/mypy --install-types --non-interactive --ignore-missing-imports --disallow-untyped-defs --follow-imports=silent --disable-error-code union-attr --disable-error-code var-annotated $1 2>&1
   error_count=$((error_count + $?))
 fi
 
 if $all
 then
   echo "${BAR}pylint..."
-  /home/mmcqueen/vicevenv/bin/pylint $1 --rcfile $CONFIG_FILE_PATH 2>&1
+  /home/mmcqueen/code/bin/pylint $1 --rcfile $CONFIG_FILE_PATH --init-hook 'import sys; sys.path.append("/home/mmcqueen/git/vice_hub/roles/ocsp2/cf/files")' 2>&1
   error_count=$((error_count + $?))
 fi
 
